5eef4237e51d3629f7778c2c53d1e10f2b49cc3e
pwnzZzZz <99125058+pwnzZzZz@users.noreply.github.com>
2023-01-06 17:05:12 +0100

    Spaß mit Spring Boot - v1.0

---
+++ Studentenverwaltung/studentenverwaltung/src/test/java/at/itkollegimst/studentenverwaltung/StudentenverwaltungApplicationTests.java
@@ -0,0 +1,13 @@
+package at.itkollegimst.studentenverwaltung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentenverwaltungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
